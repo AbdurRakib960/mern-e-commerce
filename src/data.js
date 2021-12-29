0b450948f1fc586e2d9d4597aa18da4af3f57ad8
AbdurRakib960 <rakibabdur960git@gmail.com>
2021-12-29 22:55:54 +0600

    category part done

--- src/data.js
+++ src/data.js
@@ -1,23 +1,41 @@
 export const sliderItems = [
     {
         id: 1,
-        img: "https://i.ibb.co/2vmBRq1/image-32.png",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgvPBVmtIvnL_aQpdiN7ghXVSMAmF66WIN2WYnyUMw4E6ls5h14D0AVG02JRBaqDzt9TE&usqp=CAU",
         title: "SUMMER SALE",
         desc: "DON'T COMPROMISE ON STYLE ! GET FLAT 30% OFF FOR NEW ARRIVALS",
         bg: "f5fafd"
     },
     {
         id: 2,
-        img: "https://i.ibb.co/FqcVdkJ/image-40.png",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOIZ8jmO2ENBrxlaXboKF2BCaFQ4HT95DFFQf0MEgCngHOKBgbKTvjACRZJwL_fi1dgig&usqp=CAU",
         title: "AUTUMN COLLECTION",
         desc: "DON'T COMPROMISE ON STYLE ! GET FLAT 30% OFF FOR NEW ARRIVALS",
         bg: "fcf1ed"
     },
     {
         id: 3,
-        img: "https://i.ibb.co/bbhthZB/image-39.png",
+        img: "https://image.shutterstock.com/image-photo/handsome-man-holding-shopping-bags-260nw-234381577.jpg",
         title: "POPULAR COLLECTION",
         desc: "DON'T COMPROMISE ON STYLE ! GET FLAT 30% OFF FOR NEW ARRIVALS",
         bg: "fbf0f4"
     },
-]
+];
+
+export const categories = [
+    {
+        id: 1,
+        img: "https://cf.shopee.ph/file/2cd4f8c866f5633b0309635dc2b98213",
+        title: "SHIRT STYLE"
+    },
+    {
+        id: 2,
+        img: "https://i0.wp.com/www.margaretyescombe.com/wp-content/uploads/London-Fashion-Photographer-Margaret-Yescombe-studio-Hackney-retail-imagery-for-designer-Angelily_043_sml.jpg?resize=525%2C787&ssl=1",
+        title: "LOUNGEWERE LOVE"
+    },
+    {
+        id: 3,
+        img: "https://images.pexels.com/photos/1833082/pexels-photo-1833082.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        title: "LIGHT JACKETS"
+    },
+];
