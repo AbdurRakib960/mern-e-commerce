471cdcf8032344ff4bf4de52f51fdd25f814246d
AbdurRakib960 <rakibabdur960git@gmail.com>
2021-12-29 19:56:35 +0600

    slider part completed

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://i.ibb.co/2vmBRq1/image-32.png",
+        title: "SUMMER SALE",
+        desc: "DON'T COMPROMISE ON STYLE ! GET FLAT 30% OFF FOR NEW ARRIVALS",
+        bg: "f5fafd"
+    },
+    {
+        id: 2,
+        img: "https://i.ibb.co/FqcVdkJ/image-40.png",
+        title: "AUTUMN COLLECTION",
+        desc: "DON'T COMPROMISE ON STYLE ! GET FLAT 30% OFF FOR NEW ARRIVALS",
+        bg: "fcf1ed"
+    },
+    {
+        id: 3,
+        img: "https://i.ibb.co/bbhthZB/image-39.png",
+        title: "POPULAR COLLECTION",
+        desc: "DON'T COMPROMISE ON STYLE ! GET FLAT 30% OFF FOR NEW ARRIVALS",
+        bg: "fbf0f4"
+    },
+]
